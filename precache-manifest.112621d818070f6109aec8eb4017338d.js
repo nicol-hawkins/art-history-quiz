5034bb89152e0cd266da81ed87e0f4dd4f748fa9
nicol-hawkins <nicolchawkins@gmail.com>
2020-04-28 18:51:58 -0700

    Updates

---
+++ precache-manifest.112621d818070f6109aec8eb4017338d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e062a9bef1b3a8f7446c4c43d6fcf90",
+    "url": "/art-history-quiz/index.html"
+  },
+  {
+    "revision": "50abf80479c3a600c0d0",
+    "url": "/art-history-quiz/static/css/main.1760c12f.chunk.css"
+  },
+  {
+    "revision": "f392f9b512c6e97697f3",
+    "url": "/art-history-quiz/static/js/2.63690ecd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/art-history-quiz/static/js/2.63690ecd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50abf80479c3a600c0d0",
+    "url": "/art-history-quiz/static/js/main.ea9d3a1b.chunk.js"
+  },
+  {
+    "revision": "d90a2716d5b5f37e499b",
+    "url": "/art-history-quiz/static/js/runtime-main.3a45932b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/art-history-quiz/static/media/logo.5d5d9eef.svg"
+  }
+]);
